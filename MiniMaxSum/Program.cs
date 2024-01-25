b9b7d60ab4e7d5b5874d253cda7c50d703c0f2eb
necati mert erlus <mert.erlus@outlook.com>
2024-01-25 16:48:46 +0300

    Add project files.

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long max = arr[0];
+        for (int i = 0; i < arr.Count; i++)
+            if (arr[i] > max)
+                max = arr[i];//biggest element
+
+        long min = arr[0];
+        for (int i = 0; i < arr.Count; i++)
+            if (arr[i] < min)
+                min = arr[i]; //smallest element
+
+        long biggest = 0, smallest = 0;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            smallest = smallest + arr[i];
+            biggest = biggest + arr[i];
+        }
+
+        smallest -= min;
+        biggest -= max;
+
+        Console.WriteLine(biggest + " " + smallest);
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
